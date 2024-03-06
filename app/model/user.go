54d825e461163561c8ccfbbc28a957fef73a4bc5
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-03-06 23:41:29 +0900

    Add models for test

--- app/model/user.go
+++ app/model/user.go
@@ -1,5 +1,16 @@
 package model
 
+import (
+	"time"
+)
+
+type UserID int64
+
 type User struct {
-	UserName string `json:"user_name" example:"testUser"`
+	ID       UserID    `json:"id" db:"id"`
+	Name     string    `json:"name" db:"name"`
+	Password string    `json:"password" db:"password"`
+	Role     string    `json:"role" db:"role"`
+	Created  time.Time `json:"created" db:"created"`
+	Modified time.Time `json:"modified" db:"modified"`
 }
