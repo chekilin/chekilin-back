8fff9d93a4735f90e66551bd1d97e30a38680e40
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-03-06 17:57:48 +0900

    Hide important env

--- app/config/config.go
+++ app/config/config.go
@@ -4,34 +4,35 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 
+	"github.com/caarlos0/env"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Port       int    `env:"PORT" envDefault:"80"`
+	Port       int    `env:"PORT" envDefault:"8080"`
 	DBHost     string `env:"CHEKI_DB_HOST" envDefault:"127.0.0.1"`
 	DBPort     int    `env:"CHEKI_DB_PORT" envDefault:"33306"`
-	DBUser     string `env:"CHEKI_DB_USER" envDefault:"cheki"`
-	DBPassword string `env:"CHEKI_DB_PASSWORD" envDefault:"cheki"`
+	DBUser     string `env:"CHEKI_DB_USER"`
+	DBPassword string `env:"CHEKI_DB_PASSWORD"`
 	DBName     string `env:"CHEKI_DB_NAME" envDefault:"cheki"`
 }
 
 func New() (*Config, error) {
-	err := godotenv.Load("./env/.env.dev")
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if err := godotenv.Load("env/test.env"); err != nil {
+		log.Fatal("Error loading test.env file")
 	}
 
 	cfg := &Config{}
-	portStr := os.Getenv("PORT")
-	fmt.Println(portStr)
-	port, err := strconv.Atoi(portStr)
-	if err != nil {
-		log.Fatalf("Error converting PORT to int: %v", err)
+	if err := env.Parse(cfg); err != nil {
+		fmt.Println(err)
+		return nil, err
 	}
-	cfg.Port = port
+
+	user := os.Getenv("CHEKI_DB_USER")
+	password := os.Getenv("CHEKI_DB_PASSWORD")
+	cfg.DBUser = user
+	cfg.DBPassword = password
 
 	return cfg, nil
 }
