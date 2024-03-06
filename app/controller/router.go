cd688c7112a334e1a966f9e2e03aed55a5070a13
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-03-06 14:58:01 +0900

    Add db

--- app/controller/router.go
+++ app/controller/router.go
@@ -1,16 +1,27 @@
 package controller
 
 import (
+	"cheki-back/config"
+	"cheki-back/db"
+	"context"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-var mux http.ServeMux
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
+	mux := chi.NewRouter()
+	mux.Use(middleware.Logger)
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
+	})
 
-func init() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/health", Test)
-}
+	_, cleanup, err := db.New(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
 
-func NewMux() *http.ServeMux {
-	return &mux
+	return mux, cleanup, nil
 }
